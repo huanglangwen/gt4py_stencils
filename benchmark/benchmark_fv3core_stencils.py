bfed2107d19f8bcd31589c84e5f4b77e655cee58
Langwen Huang <huanglangwen@outlook.com>
2021-01-10 16:06:25 +0100

    update

--- benchmark/benchmark_fv3core_stencils.py
+++ benchmark/benchmark_fv3core_stencils.py
@@ -3,6 +3,7 @@ try:
 except ImportError:
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from gtstencil_example import BACKEND
 from gtstencil_example.fillz import compute as fillz_compute
 from gtstencil_example.riem_solver3 import compute as riem_solver3_compute
 from gtstencil_example.riem_solver_c import compute as riem_solver_c_compute
@@ -12,6 +13,7 @@ from tests import read_data
 from tests.mockgrid import Grid
 
 from time import time
+import sys
 
 def benchmark_stencil(name, compute_func):
     etime = 0.0
@@ -25,13 +27,16 @@ def benchmark_stencil(name, compute_func):
         etime += time() - t1
     return etime
 
-def benchmark():
+def benchmark(repeat: int):
     names = ["Fillz","Riem_Solver3","Riem_Solver_C","SatAdjust3d"]
     funcs = [fillz_compute,riem_solver3_compute,riem_solver_c_compute,sat_adj_compute]
+    print(f"#Repeat times: {repeat}, backend: {BACKEND}")
     for i in range(4):
         name = names[i]
-        etime = benchmark_stencil(name, funcs[i])
+        etime = 0.0
+        for _ in range(repeat):
+            etime += benchmark_stencil(name, funcs[i])
         print(f"Stencil: {name}, time: {etime}")
 
 if __name__ == "__main__":
-    benchmark()
+    benchmark(int(sys.argv[1]))
